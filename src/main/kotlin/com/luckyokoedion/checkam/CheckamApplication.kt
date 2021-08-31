3e047ef12d0a8a10d7a226ce6c02ea30b774770e
Lucky Okoedion <luckyokoedion2000@gmail.com>
2021-08-31 01:13:02 +0100

    add all other files at first

---
+++ src/main/kotlin/com/luckyokoedion/checkam/CheckamApplication.kt
@@ -0,0 +1,10 @@
+package com.luckyokoedion.checkam
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication class CheckamApplication
+
+fun main(args: Array<String>) {
+    runApplication<CheckamApplication>(*args)
+}
---
+++ src/test/kotlin/com/luckyokoedion/checkam/CheckamApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.luckyokoedion.checkam
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CheckamApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
