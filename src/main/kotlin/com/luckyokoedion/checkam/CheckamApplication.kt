5d96eb789921b6e2b41dd7dbe6773bbd5244051e
Lucky Okoedion <luckyokoedion2000@gmail.com>
2021-09-12 14:15:10 +0100

    kjklahahiuiueuypqipn

--- src/main/kotlin/com/luckyokoedion/checkam/CheckamApplication.kt
+++ src/main/kotlin/com/luckyokoedion/checkam/CheckamApplication.kt
@@ -4,20 +4,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 
-//import org.springframework.web.bind.annotation.RequestMapping
-//import org.springframework.web.bind.annotation.ResponseBody
-//import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
 
-//@RestController
+@RestController
 
 @SpringBootApplication
 class CheckamApplication
 
-//@RequestMapping("/")
-//@ResponseBody
-//fun home(): String {
-//    return "Oh No! How did you get here? Go Back the way you came and use the api documentation";
-//}
+@RequestMapping("/")
+@ResponseBody
+fun home(): String {
+    return "Oh No! How did you get here? Go Back the way you came and use the api documentation";
+}
 
 fun main(args: Array<String>) {
     runApplication<CheckamApplication>(*args)
