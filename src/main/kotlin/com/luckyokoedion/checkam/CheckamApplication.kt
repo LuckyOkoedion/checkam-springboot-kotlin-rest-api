4f557071ad2ce2b0e5878c4c88c8a5770799a9dc
Lucky Okoedion <luckyokoedion2000@gmail.com>
2021-09-12 12:21:32 +0100

    home fix

--- src/main/kotlin/com/luckyokoedion/checkam/CheckamApplication.kt
+++ src/main/kotlin/com/luckyokoedion/checkam/CheckamApplication.kt
@@ -2,10 +2,19 @@ package com.luckyokoedion.checkam
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
 
-
+@RestController
 @SpringBootApplication
 class CheckamApplication
+
+@RequestMapping("/")
+@ResponseBody
+fun home(): String {
+    return "Oh No! How did you get here? Go Back the way you came and use the api documentation";
+}
 
 fun main(args: Array<String>) {
     runApplication<CheckamApplication>(*args)
